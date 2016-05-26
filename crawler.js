c64f4498c2b3324bbedc67d1d8e1aafc82323a25
Ahertig <alyssa.hertig@gmail.com>
2016-05-26 17:59:50 -0400

    Turn web crawler into a microservice

--- crawler.js
+++ crawler.js
@@ -2,34 +2,42 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-
 var pageToVisit = "http://www.pitchfork.com/";
 var pageToVisit2 = "http://calipermusic.blogspot.com/";
 
-console.log("Visiting page " + pageToVisit);
 
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
+module.exports = function(options) {
+  var seneca = this;
 
-     var albumsAndArtists = [];
+  seneca.add({role:'crawler', cmd:'find_albums', find_albums});
 
-     $('.album-details ul').each(function(i, elem) {
-        albumsAndArtists[i] = $(this).text();
-     });
+  function find_albums(done) {
+    // Is this function within a function going to work? :-/
+    request(pageToVisit, function(error, response, body) {
+       if(error) {
+         console.log("Error: " + error);
+       }
+       // Check status code (200 is HTTP OK)
+       console.log("Status code: " + response.statusCode);
+       if(response.statusCode === 200) {
+         // Parse the document body
+         var $ = cheerio.load(body);
+         console.log("Page title:  " + $('title').text());
 
-     console.log("Album titles: " + albumsAndArtists);
-   }
-});
+         var albumsAndArtists = [];
 
+         $('.album-details ul').each(function(i, elem) {
+            albumsAndArtists[i] = $(this).text();
+         });
+         // What's the purpose of this in Seneca???
+         // Is it passing albumsAndArtists for use elsewhere?
+        done(null, albumsAndArtists);
+       }
 
+    });
+  }
+  
+}
 
 
 // function collectInternalLinks($) {
