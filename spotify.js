cb430d6cdee42421740969f6f9ad66840b2981c7
Ahertig <alyssa.hertig@gmail.com>
2016-05-29 20:01:47 -0400

    Set up express and client information authentication

---
+++ spotify.js
@@ -0,0 +1,26 @@
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var app = express();
+
+var client_id = 'ed4c50af50df42e58a44b1ef421ba0e9'; // Your client id
+var client_secret = '8800ba4c79844a48b7a9212fbfce191f'; // Your secret
+var redirect_uri = '/callback'; // Your redirect uri
+
+// Authenticate user so that we can add music to their playlist
+
+// Pass whatever the crawler results over to spotify
+
+// Browse for artists
+
+// if found, then find one top track
+
+// Add it to a Spotify playlist
+
+
+
+
+
+
+
+console.log('Listening on 8000');
+app.listen(8000);
